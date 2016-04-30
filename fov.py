e43e546ce7f4a45de3d4a0a802e564a19db4287b
Kushal gandhi <kushalgandhi@Kushals-MacBook-Pro.local>
2016-04-30 14:33:10 +0530

    Added lines to write into database

--- fov.py
+++ fov.py
@@ -17,3 +17,9 @@ var1["education"] = var2["2010"]
 var1["cashSurplusDeficit"] = var3["2010"]
 
 var1.drop(var1.columns[range(2,4)], axis = 1, inplace = True)
+var1.rename(columns={"Country Name": "CountryName"}, inplace=True)
+var1.rename(columns={"Country Code": "CountryCode"}, inplace=True)
+
+engine = create_engine('mysql+mysqlconnector://root@localhost:3306/fov', echo=False)
+cnx = engine.raw_connection()
+var1.to_sql(name='masterdata', con=cnx, if_exists = 'append', flavor = "mysql", index=False)
